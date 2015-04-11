c1c80b34a5ffe513d30b12fb1dc154cf91856138
Liu Liu <lllxjy2009@gmail.com>
2015-04-11 14:34:58 -0700

    update button

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 
 
-$( "body" ).append( "<button id='magic' class='hide' style='z-index: 1000;position: absolute;left: 0;bottom: 0;height: 20px;'>Liu's magic</button>" );
+$( "body" ).append( "<button id='magic' class='hide' style='z-index: 1000;position: absolute;left: 0;bottom: 0;height: 24px; width: 24px; border:none; border-radius: 50px; background: #4285F4;'></button>" );
 
 $('.hide').click(function(){
 	$('#docs-chrome').toggle();
