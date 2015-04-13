fa6672b0ea87296b6398265599c190d4586386b5
Liu Liu <lllxjy2009@gmail.com>
2015-04-12 17:53:51 -0700

    add icon

--- package/main.js
+++ package/main.js
@@ -1,6 +1,6 @@
 
 
-$( "body" ).append( "<button id='magic' class='hide' style='z-index: 1000;position: absolute;left: 8px;bottom: 8px;height: 24px; width: 24px; border:none; border-radius: 50px; background: #F57F17;cursor:pointer;outline:none'></button>" );
+$( "body" ).append( "<button id='magic' class='hide' style='z-index: 1000;position: absolute;left: 8px;bottom: 8px;height: 36px; width: 36px; border:none; border-radius: 50px; background: #F57F17;cursor:pointer;outline:none;box-shadow: 0 2px 5px 0 rgba(0,0,0,.16), 0 2px 5px 0 rgba(0,0,0,.23);'><img src='chrome-extension://iholdcogllchfpmdnkiaaobcghmbgblf/icon.svg'></button>" );
 
 $('.hide').click(function(){
 	$('#docs-chrome').toggle();
